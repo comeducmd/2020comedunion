95a397a9e22d2131eb1e2c702de1d41d166597c3
yj <yoonjaebaek0312@gmail.com>
2020-11-06 23:26:37 +0900

    [FEAT] Change Database to MySQL

--- config/settings.py
+++ config/settings.py
@@ -73,13 +73,17 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+"""
+import my_settings
 
+DATABASES = my_settings.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
