3b9ff3ccdc8e0a366f62a6574465f1d178f1db89
SeojinSeojin <1106laura@naver.com>
2020-11-14 23:52:20 +0900

    [feat] 추천수 model

--- recommends/models.py
+++ recommends/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Likes(models.Model):
+    """
+    How many likes did the post got?
+    """
+    
+    liked_user = models.ManyToManyField("users.User", related_name="like")
+    likes = models.IntegerField(default=0)
+    liked_post = models.OneToOneField("posts.Post", on_delete=models.CASCADE)
+
+    def count_likes(self):
+        likes = self.liked_user.count()
+        return likes
