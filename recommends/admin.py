df331040e373a457e991e86eb540ff2b91f932ca
yj <yoonjaebaek0312@gmail.com>
2020-11-15 23:46:39 +0900

    [FEAT] recommends model migration & admin

--- recommends/admin.py
+++ recommends/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Likes)
+class RecommendAdmin(admin.ModelAdmin):
+    """Custom Likes Admin"""
