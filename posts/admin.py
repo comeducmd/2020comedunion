85855121fac00a53f56e1b76061e4173daab8452
yj <yoonjaebaek0312@gmail.com>
2020-11-15 23:47:51 +0900

    [FEAT] Post model & admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    """Custom Post Admin """
+
+    list_display = (
+        "title",
+        "team_name",
+        "count_likes",
+        "pub_date",
+    )
+
+    list_filter = ("team_name",)
+
+    def count_likes(self, obj):
+        return obj.likes.count_likes()
