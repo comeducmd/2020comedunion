7f0258d60ade0164c477739c64cb2e86ec9ee190
yj <yoonjaebaek0312@gmail.com>
2020-11-18 23:21:29 +0900

    [FIX] URL & Django Template Setting - 1/N

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from . import models
 
 # Create your views here.
+
+
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Post
+    paginate_by = 6
+    paginate_orphans = 5
+    ordering = "pub_date"
+    context_object_name = "posts"
