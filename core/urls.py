b2318764ccc216ab5f262fd5facd686e94147608
yj <yoonjaebaek0312@gmail.com>
2020-11-29 01:16:59 +0900

    [FEAT] Intro - view, template, urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from . import views as core_views
 from posts import views as post_views
 
 app_name = "core"
 
-urlpatterns = [path("", post_views.HomeView.as_view(), name="home")]
+urlpatterns = [
+    path("", core_views.IntroView, name="intro"),
+    path("home", post_views.HomeView.as_view(), name="home"),
+]
