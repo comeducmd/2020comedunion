6528faaf4bbddefa809b3fe4a29f26e6bc26e856
yj <yoonjaebaek0312@gmail.com>
2020-12-02 23:59:00 +0900

    [FEAT] Login Page, Kakao Login Link Connection

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from core import views as core_views
 
 app_name = "users"
 
 urlpatterns = [
+    path("login/", core_views.LoginPageView, name="login"),
     path("login/kakao/", views.kakao_login, name="kakao-login"),
     path("login/kakao/callback/", views.kakao_callback, name="kakao-callback"),
 ]
