aee466af53fa4aff1ed3ba19db340e3d72d41d60
yj <yoonjaebaek0312@gmail.com>
2020-12-19 02:51:50 +0900

    [FIX] User model team member -> charfield

--- posts/models.py
+++ posts/models.py
@@ -21,11 +21,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=200)
     team_name = models.CharField(max_length=100)
-    team_member = models.ManyToManyField(
-        "users.User",
-        related_name="posts",
-        blank=True,
-    )
+    team_member = models.CharField(max_length=200)
     pub_date = models.DateTimeField(default=timezone.now)
     pub_url = models.URLField(blank=True)
     video_url = models.URLField(blank=True)
