3e0fb2a938614ed4f6f3fdbcb21c74d42e0850ad
SeojinSeojin <1106laura@naver.com>
2020-12-20 10:45:41 +0900

    [fix] 모달 수정

--- static/js/handle-modal.js
+++ static/js/handle-modal.js
@@ -5,14 +5,19 @@ $(document).ready(function() {
         $(`div.popupDiv.popUp-${thisId}`).css({
             "top": (($(window).height() - $(`div.popupDiv.popUp-${thisId}`).outerHeight()) / 2 + $(window).scrollTop()) + "px",
             "left": (($(window).width() - $(`div.popupDiv.popUp-${thisId}`).outerWidth()) / 2 + $(window).scrollLeft()) + "px",
-            "display": "block"
         });
-        $(".popupMask").css("display", "block");
+        $(`div.popupDiv.popUp-${thisId}`).fadeIn()
+        $(".popupMask").fadeIn('fast');
         $("body").css("overflow", "hidden");
         $(`.popCloseBtn-${thisId}`).css("cursor", "pointer");
         $(`.popCloseBtn-${thisId}`).click(function(event) {
-            $(".popupMask").css("display", "none");
-            $(`div.popupDiv.popUp-${thisId}`).css("display", "none");
+            $(".popupMask").fadeOut();
+            $(`div.popupDiv.popUp-${thisId}`).fadeOut('fast');
+            $("body").css("overflow", "auto");
+        });
+        $(".popupMask").click(function(event) {
+            $(".popupMask").fadeOut();
+            $(`div.popupDiv.popUp-${thisId}`).fadeOut('fast');
             $("body").css("overflow", "auto");
         });
     });
